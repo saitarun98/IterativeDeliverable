51d8becb35eb6c73d2d79091a163d04eb3d098d8
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-24 17:24:34 -0500

    v1.0

--- IterativeDeliverable/Program.cs
+++ IterativeDeliverable/Program.cs
@@ -6,7 +6,43 @@ namespace IterativeDeliverable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hi! Please enter an integer between 1 and 100.  ");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+
+                if (value_of_input >= 1 && value_of_input <= 100)
+                {
+                    Console.WriteLine("Executing a For Loop!");
+                    Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + " times!");
+
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("You have entered " + value_of_input.ToString() + ". " +
+                            "This is the current integer value in the loop: " + i.ToString());
+                    }
+
+                    Console.WriteLine("Press any key to exit the program.");
+                    Console.ReadKey(true);
+                }//end of do while loop
+
+                else
+                {
+                    Console.WriteLine("Error! Please enter a valid number between 1-100.");
+                    Console.WriteLine("Please press any key to exit and try again.");
+                    Console.ReadKey(true);
+                }
+            } //End of try
+
+            catch
+            {
+                Console.WriteLine("Error! Please enter a valid number between 1-100.");
+                Console.WriteLine("Please press any key to exit and try again.");
+                Console.ReadKey(true);
+            }//End of catch
         }
     }
 }
+
