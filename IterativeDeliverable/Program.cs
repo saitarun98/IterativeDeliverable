43ab0fb43ceb96c108d7f1481a5d229c5233f0c4
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-24 17:33:42 -0500

    v1.0

--- IterativeDeliverable/Program.cs
+++ IterativeDeliverable/Program.cs
@@ -7,7 +7,8 @@ namespace IterativeDeliverable
         static void Main(string[] args)
         {
             Console.Write("Hi! Please enter an integer between 1 and 100.  ");
-
+            
+            //Start of try statement
             try
             {
                 string input = Console.ReadLine();
@@ -26,14 +27,15 @@ namespace IterativeDeliverable
 
                     Console.WriteLine("Press any key to exit the program.");
                     Console.ReadKey(true);
-                }//end of do while loop
+                }//end of for loop, end of if statement
 
+                //If the intger is over 100
                 else
                 {
                     Console.WriteLine("Error! Please enter a valid number between 1-100.");
                     Console.WriteLine("Please press any key to exit and try again.");
                     Console.ReadKey(true);
-                }
+                }//end of else statement
             } //End of try
 
             catch
